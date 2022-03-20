f09020a39e7d779dcaa6b863a4fe9862c9635b7b
Eric Murray <mailboxe@mac.com>
2022-03-19 17:46:51 -0700

    added root package.json for heroku deployment

--- build/static/js/googlePopup.js
+++
@@ -1,9 +0,0 @@
-function showGooglePopup(event) {
-  event.preventDefault();
-  const googleURL = "http://localhost:8081/auth/google";
-  const newWindow = window.open(
-    googleURL,
-    "_blank",
-    "toolbar=no, menubar=no, width=600, height=700, top=100, left=100"
-  );
-}
